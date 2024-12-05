82f3af8090b32bffada21da14dcb17ddbafe855e
장연식 <sseregit@knowwheresoft.com>
2024-12-05 16:59:52 +0900

    나누기 연산의 원리

---
+++ src/devideSample.java
@@ -0,0 +1,7 @@
+public class devideSample {
+    public static void main(String[] args) {
+        System.out.printf("%d\n", 5 / 2);
+        System.out.printf("%d\n", 5 % 2);
+        System.out.printf("%f\n", 5.0 / 2);
+    }
+}
