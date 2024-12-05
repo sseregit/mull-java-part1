2ab7419a707f4dd64ef398558c5fe8dc28636460
장연식 <sseregit@knowwheresoft.com>
2024-12-05 13:34:06 +0900

    Scanner를 이용한 정수, 실수 입/출력

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        int keycode;
-        keycode = System.in.read();
-        System.out.println(keycode);
-        keycode = System.in.read();
-        System.out.println(keycode);
-        keycode = System.in.read();
-        System.out.println(keycode);
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = 0;
+        try {
+            a = s.nextInt();
+            System.out.println(a);
+        } catch (Exception e) {
+            System.out.println("ERROR");
+        }
+        System.out.println("End");
     }
 }
