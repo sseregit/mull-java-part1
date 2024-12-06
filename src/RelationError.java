c90895aa304d9f29af430ac733e1b8701750c489
장연식 <sseregit@knowwheresoft.com>
2024-12-06 11:40:21 +0900

    관계 연산자

---
+++ src/RelationError.java
@@ -0,0 +1,8 @@
+public class RelationError {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(300 == 299.999998F);
+        System.out.println(300 == 299.999999);
+//        System.out.println(5 < a < 15);
+    }
+}
---
+++ src/RelationSample.java
@@ -0,0 +1,11 @@
+public class RelationSample {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > 5);
+        System.out.println(b < 5);
+        System.out.println(b >= 10);
+        System.out.println(a <= a + 5);
+    }
+}
